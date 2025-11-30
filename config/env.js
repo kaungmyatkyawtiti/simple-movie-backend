9ee541ddcd05feb77b1c9e19ee8bef452b608d10
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-11-30 12:30:55 +0630

    chore: push project to github

--- config/env.js
+++ config/env.js
@@ -5,6 +5,6 @@ module.exports = {
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
-  PORT: process.env.PORT || 3000,
+  PORT: process.env.PORT || 8000,
   LOG_ENABLED: process.env.LOG_ENABLED,
 };
